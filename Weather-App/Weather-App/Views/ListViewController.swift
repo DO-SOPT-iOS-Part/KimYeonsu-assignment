7b93524c7652eeb7bc352c05d00fb43ea14b5eef
yeonsu <drawingbonny@gmail.com>
2023-11-08 01:18:10 +0900

    feat: Add initial settings

---
+++ Weather-App/Weather-App/Views/ListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ListViewController.swift
+//  Weather-App
+//
+//  Created by yeonsu on 11/8/23.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
